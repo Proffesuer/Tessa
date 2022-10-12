f5ba99bef5cdcad7f4dff7b8b348f245d524f817
Proffesuer <levischisira0@gmail.com>
2022-10-12 16:07:59 +0300

    Created an JSX element and rendered it with a root id

---
+++ tessas/src/index.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+const element=<h1>Hello world my people its chisira!!</h1>;
+console.log(element);
+reactDOM.render(element,document.getElementById('root'));
