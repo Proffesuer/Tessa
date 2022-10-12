0afdde5a9cc3f2bebfd5b34698a59d4ab66655a0
Proffesuer <levischisira0@gmail.com>
2022-10-12 16:23:18 +0300

    finished my hello world example

--- tessas/src/index.js
+++ tessas/src/index.js
@@ -1,6 +1,16 @@
+//we need to import the react object from the react module
 import react from 'react';
+//we also need to import reactDom from react-dom module
 import reactDOM from 'react-dom';
 
+//create an element with JSX code that would be automatically converted to javascript automatically 
 const element=<h1>Hello world my people its chisira!!</h1>;
-console.log(element);
+
+
+//console.log(element);
+
+
+
+//with a reactDom we nned to set our element on an id
+
 reactDOM.render(element,document.getElementById('root'));
