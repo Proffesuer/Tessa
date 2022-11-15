d09bbb62d654d2f15edcf6ccbd785dfc1ebe9224
Proffesuer <levischisira0@gmail.com>
2022-11-15 23:40:02 +0300

    Embedded some expressions

--- tessas/src/my_component/counter.jsx
+++ tessas/src/my_component/counter.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-   
+   state={
+    count:0
+
+   };
     render() { 
-        return <h1>Hello world</h1>;
-    }
+        return (<React.Fragment>
+            <span>{this.formatcount()}</span>
+        <button>Increment</button></React.Fragment>
+        );
 }
- 
+
+formatcount(){
+
+const {count} =this.state;
+    return count === 0 ?Zero :count;
+}
+}
+
+
 export default Counter;
