2b56da2c5418c7ea37ad45c0be6a4ca99e17819f
Proffesuer <levischisira0@gmail.com>
2022-12-27 23:13:44 +0300

    added and tested bootstrap in my application

--- tessas/src/my_component/counter.jsx
+++ tessas/src/my_component/counter.jsx
@@ -10,9 +10,22 @@ count:0,
     render() { 
         return (
         <React.Fragment>
-        
+        <div className='container'>
+            <div className='row'>
+                <div className='col-sm-6'>
             <span className='badge badge-primary m-2 bg-success'>{this.formatCount()}</span>
             <button>Increment</button>
+            <p>hello  this is my first column on my first row guys thats is really great and am really proud about it that am keeping moving </p>
+            </div>
+        <div className='col-sm-5'>
+        <span className='badge badge-primary m-2 bg-success'>{this.formatCount()}</span>
+            <button>Increment</button>
+            <p>this is my second column on my first row guys and am really pround that i keep moving </p>
+
+
+        </div>
+            </div>
+            </div>
 
             </React.Fragment>
         );
